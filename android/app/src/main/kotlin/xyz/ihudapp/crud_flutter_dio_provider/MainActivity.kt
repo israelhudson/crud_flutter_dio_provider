894bfc2423124b6e08496d817e3fea9a1d5cd4aa
Israel Hudson <israelhudson@gmail.com>
2019-08-29 15:26:20 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/xyz/ihudapp/crud_flutter_dio_provider/MainActivity.kt
@@ -0,0 +1,13 @@
+package xyz.ihudapp.crud_flutter_dio_provider
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
